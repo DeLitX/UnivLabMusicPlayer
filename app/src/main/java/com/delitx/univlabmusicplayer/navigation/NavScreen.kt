4c66edd33738465737e27c55cf0236350ba0c6a8
DeLitX <dimkanan@gmail.com>
2023-11-19 20:32:16 +0200

    Added bottom bar

--- app/src/main/java/com/delitx/univlabmusicplayer/navigation/NavScreen.kt
+++ app/src/main/java/com/delitx/univlabmusicplayer/navigation/NavScreen.kt
@@ -2,20 +2,25 @@ package com.delitx.univlabmusicplayer.navigation
 
 sealed class NavScreen {
     abstract val route: String
+    abstract val routePrefix: String
 
     object AllAudio : NavScreen() {
         override val route: String = "all_audio_screen"
+        override val routePrefix: String = route
     }
 
     object Playlists : NavScreen() {
         override val route: String = "playlists_screen"
+        override val routePrefix: String = route
     }
 
     object CurrentAudio : NavScreen() {
         override val route: String = "current_audio_screen"
+        override val routePrefix: String = route
     }
 
     data class Playlist(val playlistId: Int) : NavScreen() {
         override val route: String = "playlist_screen?playlist_id=$playlistId"
+        override val routePrefix: String = "playlist_screen"
     }
 }
